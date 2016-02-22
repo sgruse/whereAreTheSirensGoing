b95960f9b2c51ce69cff88875fcc6d44280c5090
Sam Gruse <sgruse89@gmail.com>
2016-02-22 13:28:00 -0800

    finished setting up data fetcher

--- js/dataFetcher.js
+++ js/dataFetcher.js
@@ -7,27 +7,32 @@
     $.ajax({
         url: 'https://data.seattle.gov/resource/3k2p-39jp.json'
             //What to search for.
-          + '?event_clearance_code=470'
+          + '?event_clearance_code=031'
             //Parameters of Search.
           + '&$order=event_clearance_date DESC',
       type: 'GET',
       ContentType: 'json',
       headers: { 'X-App-Token': appToken },
       success: function(data, message, xhr){
-        console.log(message);
         console.log(data);
+        console.log(data.length);
+        dataFetcher.parseData(data);
       },
       error: function(){
         console.log('error recieveing data');
       }
-    })
+    });
   };
 
-  dataFetcher.parseData = function(){
-    
+  dataFetcher.parseData = function(data){
+    data.filter(function(el){
+      // console.log(el.latitude);
+      // console.log(el.event_clearance_group);
+      // console.log(el);
+    });
   };
 
-dataFetcher.fetchData();
+  dataFetcher.fetchData();
 
   module.dataFetcher = dataFetcher;
 })(window);
