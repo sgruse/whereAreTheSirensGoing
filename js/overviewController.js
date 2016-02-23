288324a13c15c655425e1fbdbb2f853649455af6
Hugo A Guerra <guerrahugoa@gmail.com>
2016-02-23 10:33:48 -0800

    initialized overviewController.js, added logic to call overview data using set map coordinates

---
+++ js/overviewController.js
@@ -0,0 +1,31 @@
+(function(module){
+  overviewController = {};
+
+  //code below could be refactored, using dataFetcher to make the ajax call
+  overviewController.callBasicData = function() {
+
+    var latitude = 47.61;
+    var longitude = -122.34;
+
+    $.ajax({
+      url: 'https://data.seattle.gov/resource/3k2p-39jp.json'
+          + '?$order=event_clearance_date DESC'
+          + 'where=within_circle(incident_location,' + latitude + ',' + longitude + ',10000)',
+      type: 'GET',
+      ContentType: 'json',
+      headers: { 'X-App-Token': appToken },
+      success: function(data, message, xhr) {
+        dataFetcher.parseData(data);
+        ctx.handled = true;
+        next();
+      },
+      error: function(){
+        console.log('error in processing overviewController.callBasicData data');
+        ctx.handled = true;
+        next();
+      }
+    });
+  };
+
+
+})(window);
