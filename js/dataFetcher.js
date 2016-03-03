f6712d95c70f953512a75b523094b0d7c563de52
Frazier Mork <fraziermork@gmail.com>
2016-03-03 10:03:16 -0800

    removed commented out code

--- js/dataFetcher.js
+++ js/dataFetcher.js
@@ -2,9 +2,6 @@
 
   var appToken = '0vBJ6JiStgBAfDkeAJf5h7645';
   var dataFetcher = {};
-  
-  //deprecated
-  // dataFetcher.filterArray = [['violent-crimes', 'Violent Crimes', ['090', '091', '092', '040', '043', '049', '041', '026', '242', '249', '179', '010', '510', '291', '292', '330', '051', '052']], ['non-violent','Non-Violent Crimes',['050', '053', '160', '161']],['sex-crimes','Sex Crimes',['020', '021', '141', '142']],['vehicle','Auto Crimes',['071', '072', '074', '063', '061', '062']],['quality','Quality of Life',['130', '139','174', '176', '177', '080', '081', '082', '083', '084', '085', '086', '087', '243', '244', '245', '246', '170', '171', '220', '221', '181', '182', '183', '184', '450', '150', '151', '152', '125', '127']]];
 
   dataFetcher.filterCodes = {
     violentCrimes: ['090', '091', '092', '040', '043', '049', '041', '026', '242', '249', '179', '010', '510', '291', '292', '330', '051', '052'],
@@ -13,26 +10,7 @@
     sexCrimes: ['020', '021', '141', '142'],
     qualityOfLifeCrimes: ['130', '139','174', '176', '177', '080', '081', '082', '083', '084', '085', '086', '087', '243', '244', '245', '246', '170', '171', '220', '221', '181', '182', '183', '184', '450', '150', '151', '152', '125', '127']
   };
-
-  //deprecated
-  // dataFetcher.formatUrlForApi = function(parameterObj, additionalWhere){
-  //   console.log('dataFetcher.formatUrlForApi');
-  //   var latitude, longitude;
-  //   var formattedUrl = 'https://data.seattle.gov/resource/pu5n-trf4.json';
-  //   formattedUrl += '?&$order=event_clearance_date DESC';
-  //   var whereClause = "&$where=event_clearance_code in ('090', '091', '092', '040', '043', '049', '041', '026', '242', '249', '179', '010', '510', '291', '292', '330', '051', '052', '050', '053', '160', '161', '020', '021', '141', '142', '071', '072', '074', '063', '061', '062', '130', '139','174', '176', '177', '080', '081', '082', '083', '084', '085', '086', '087', '243', '244', '245', '246', '170', '171', '220', '221', '181', '182', '183', '184', '450', '150', '151', '152', '125', '127')";
-  //   if (parameterObj.lat && parameterObj.lng){
-  //     whereClause += 'AND within_circle(incident_location,'+ parameterObj.lat + ',' + parameterObj.lng + ',1000)'; //stretch goal: make the 10000 a variable passed in through the parameters so that it can be related to google maps zoom
-  //   }
-  //   if (additionalWhere){
-  //     whereClause += 'AND ' + additionalWhere;
-  //   }
-  //   console.log(whereClause);
-  //   formattedUrl += whereClause;
-  //   console.log(formattedUrl);
-  //   return formattedUrl;
-  // };
-  
+    
   //This builds the URL for the new API implementation on form changes
   dataFetcher.formatSpecificUrlForApi = function(parameterObj){
     console.log('dataFetcher.formatSpecificUrlForApi called');
